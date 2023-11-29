9560cb8cb4d6cff93a2e28839cc749216ee5b212
Apps816 <123339291+ry-bro-uri@users.noreply.github.com>
2023-11-28 21:53:07 -0500

    Update execution.rs

--- rumdump/src/execution.rs
+++ rumdump/src/execution.rs
@@ -1,4 +1,4 @@
-use crate::memory::{Memory, Registers};
+use crate::memory::{Memory, Registers, self};
 
 pub fn cmov(registers: &mut Registers, a: u32, b: u32, c: u32) {
     let a = registers.data[a as usize];
@@ -10,12 +10,28 @@ pub fn cmov(registers: &mut Registers, a: u32, b: u32, c: u32) {
     }
 }
 
-pub fn load() {
+pub fn load(registers: &mut Registers, memory: &mut Memory, a: u32, b: u32, c: u32) {
+    let a = registers.data[a as usize];
+    let b = registers.data[b as usize];
+    let c = registers.data[c as usize];
 
+    if let Some(segment) = memory.get(&(registers.data[b as usize])) {
+        if let Some(value) = segment.get(registers.data[c as usize] as usize) {
+            registers.data[a as usize] = *value;
+        }
+    }
 }
 
-pub fn store() {
+pub fn store(registers: &mut Registers, memory: Memory, a: u32, b: u32, c: u32) {
+    let a = registers.data[a as usize];
+    let b = registers.data[b as usize];
+    let c = registers.data[c as usize];
 
+    if let Some(segment) = memory.get(&(registers.data[a as usize])) {
+        if let Some(mut value) = segment.get(registers.data[b as usize] as usize) {
+            value = &registers.data[c as usize];
+        }
+    }
 }
 
 pub fn add(registers: &mut Registers, a: u32, b: u32, c: u32) {
